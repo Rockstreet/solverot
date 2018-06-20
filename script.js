c18b85380299d09b86064ff6f4c5c0c568b88448
Trump <trumpk@gmail.com>
2018-06-20 20:06:23 +0300

    All pages

--- script.js
+++ script.js
@@ -7,11 +7,35 @@ if(path=="") path='1';
 
 prev_path = "page" + (parseInt(path) - 1) + ".html";
 next_path = "page" + (parseInt(path) + 1) + ".html";
-console.log(next_path);
+//console.log(next_path);
 
 function prev() {
     top.location.href = prev_path;
 }
 function next() {
     top.location.href = next_path;
+}
+
+
+
+function loadFunc() {
+    var div = document.createElement('div');
+    div.className = "home_link";
+    div.innerHTML = "<strong>Оглавление</strong>";
+    document.body.appendChild(div);
+}
+
+if(window.attachEvent) {
+    window.attachEvent('onload', loadFunc);
+} else {
+    if(window.onload) {
+        var curronload = window.onload;
+        var newonload = function(evt) {
+            curronload(evt);
+            yourFunctionName(evt);
+        };
+        window.onload = newonload;
+    } else {
+        window.onload = loadFunc;
+    }
 }
