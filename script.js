d43ddcbedf7602fb3cc52daf7aed632ae5f358ad
denis <trumpk@gmail.com>
2018-06-21 14:40:49 +0300

    all changes

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ if(path=="") path='1';
 
 prev_path = "page" + (parseInt(path) - 1) + ".html";
 next_path = "page" + (parseInt(path) + 1) + ".html";
-//console.log(next_path);
+console.log(path);
 
 function prev() {
     top.location.href = prev_path;
@@ -21,8 +21,8 @@ function next() {
 function loadFunc() {
     var div = document.createElement('div');
     div.className = "home_link";
-    div.innerHTML = "<strong>Оглавление</strong>";
-    document.body.appendChild(div);
+    div.innerHTML = "<a href='./index.html'><img src='./img/home.png' class='d-none d-md-block' width='100' alt='В оглавление' title='В оглавление'><img src='./img/home.png' class='d-block d-md-none' width='50' alt='В оглавление' title='В оглавление'></a>";
+    if (path != 'index') document.body.appendChild(div);
 }
 
 if(window.attachEvent) {
